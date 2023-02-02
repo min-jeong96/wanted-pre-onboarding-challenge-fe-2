6d94deed5a95583593b16f22900ec531080cd0d0
min-jeong96 <minjeong.dev@gmail.com>
2023-02-02 21:12:35 +0900

    fix: Todo type -> class로 수정

--- src/todo.js
+++ src/todo.js
@@ -1,15 +1,11 @@
 /**
- * Todo
- * @typedef {Object} Todo
- * @property {number} id - Todo 아이디
- * @property {string} content - Todo 내용
- * @property {boolean} completed - Todo 완료 여부
- * @property {string} category - Todo 카테고리
- * @property {string[]} [tags] - Todo 태그들
+ * 할 일
+ * @constructor
+ * @type {Object}
+ * @property {number} id - 아이디
+ * @property {string} content - 내용
+ * @property {boolean} completed - 완료 여부
+ * @property {string} category - 카테고리
+ * @property {string[]} [tags] - 태그들
  */
-
-/**
- * Todos
- * @type {Todo[]}
- */
-const todos = [];
+function Todo(id, content, completed, category, tags) { }
